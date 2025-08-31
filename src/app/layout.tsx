92d4769ac71beb1d1ae87247c78e4dde33cf9ae9
Saksham Verma <sakshamverma1000@gmail.com>
2025-08-31 12:58:25 +0530

    Added clerk authentication, did not create the frontend for it yet.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,14 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { Toaster } from "@/components/ui/sonner"
-
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 const dmSans = DM_Sans({subsets:['latin']});
 
 const geistSans = Geist({
@@ -28,10 +35,24 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
     <html lang="en" suppressHydrationWarning>
       <body suppressHydrationWarning
         className={`${dmSans.className} ${dmSans.className} antialiased`}
       >
+        <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
        
         <ThemeProvider attribute = "class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           {children}
@@ -39,5 +60,6 @@ export default function RootLayout({
         </ThemeProvider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -1,3 +1,4 @@
+import { UserButton } from '@clerk/nextjs'
 import { MenuIcon } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -62,7 +63,8 @@ const Navbar = async() => {
             {
                 // WIP: Wire up User
             }
-
+            
+            <UserButton/>
             <MenuIcon className='md:hidden'/>
         </aside>
     </header>
