41f84c56c217889ed5125264469d0526a0e05a4a
Saksham Verma <sakshamverma1000@gmail.com>
2025-09-09 10:14:41 +0530

    Error was that i wasn't verifying the clerk webhook header signature, now i'm doing that using Svix. Right now it's working for Clerk Dashboard Test Events, will make it work for real signpus later on

--- src/app/api/clerk-webhook/route.ts
+++ src/app/api/clerk-webhook/route.ts
@@ -1,19 +1,61 @@
 import { db } from "@/lib/db";
 import { NextRequest, NextResponse } from "next/server";
+import { Webhook } from "svix";
+import {headers} from "next/headers";
 
 
-export async function POST(req:Request){
+type ClerkUserWebhookEvent = {
+    data:{
+        id: string;
+        email_addresses: {
+            email_address: string
+        }[];
 
+        first_name: string | null;
+        last_name: string | null;
+        image_url: string | null;
+    };
+
+    object: string;
+    type: string;
+}
+export async function POST(req:NextRequest){
+
+    // WIP: The user is not getting Upsert into the DB.
     try{
 
-        const body = await req.json();
-        const {id,email_addresses, first_name,image_url} = body?.data;
+        const payload = await req.text(); // raw body
+        const headerPayload = await headers();
 
-        const email = email_addresses[0]?.email_address
+
+        if(!process.env.CLERK_WEBHOOK_SECRET){
+            console.error("\nCouldn't access Clerk Webhook Secret\n");
+            return NextResponse.json({message:"Clerk Webhook Secret couldn't be accessed"}, {status:400});
+        }
+
+        const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+        const evt = wh.verify(payload,{
+            "svix-id": headerPayload.get("svix-id")!,
+            "svix-timestamp": headerPayload.get("svix-timestamp")!,
+            "svix-signature": headerPayload.get("svix-signature")!,
+
+        }) as ClerkUserWebhookEvent;
+
+        // const body = await req.json();
+        const {id,email_addresses, first_name,image_url} = evt.data;
+
+        let email = email_addresses[0]?.email_address
+
+        if(!email){
+            email = "";
+            console.warn("Test event sent by clerk without any email");
+            // return NextResponse.json({message:"Email coudln't be accessed"}, {status:400});
+        }
+
         console.log("Clerk-Webhook endpiont was called ✅")
         
 
-        await db.user.upsert({
+        const dbResponse = await db.user.upsert({
             where: {clerkId: id},
             update:{
                 email,
@@ -29,11 +71,17 @@ export async function POST(req:Request){
             }
         });
 
+        if(!dbResponse){
+            console.error("\nDB upsert() error\n");
+            return NextResponse.json({message:"Database upsert() error"}, {status:400});
+        }
+
+        console.log("\nUser was created successfully ✅\n");
 
         return NextResponse.json({message:"User updated successfully in Database"},{status:200});
 
     }catch(err){
-        console.log(err);
+        console.log("Error while executing code of /api/clerk-webhook route.ts file",err);
         return NextResponse.json({message:"Internal Server Error"}, {status:500});
     }
 }
